7dd838e298ca12b16c96d57e750938359697b40d
TannerGiddings21 <105750510+TannerGiddings21@users.noreply.github.com>
2022-06-13 09:19:10 -0400

    Create Instructor.java

---
+++ Deliverable11/app/src/main/java/com/example/deliverable11/Instructor.java
@@ -0,0 +1,8 @@
+package com.example.deliverable11;
+
+public class Instructor extends User{
+
+    public Instructor(String username, String password, int id) {
+        super(username, password, id);
+    }
+}
