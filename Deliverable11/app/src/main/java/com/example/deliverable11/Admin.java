8b999940524b709d420a7e69f8c40ac8ad66259f
TannerGiddings21 <105750510+TannerGiddings21@users.noreply.github.com>
2022-06-13 15:05:19 -0400

    Update Admin.java

--- Deliverable11/app/src/main/java/com/example/deliverable11/Admin.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/Admin.java
@@ -2,8 +2,8 @@ package com.example.deliverable11;
 
 public class Admin extends User{
 
-    public Admin(String username, String password, int id) {
-        super(username, password, id);
+    public Admin(String username, String password) {
+        super(username, password);
     }
     
 }
