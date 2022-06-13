61c16e14fc92ad31316891a8cd02aa0eedf45619
laure <lnshi006@uottawa.ca>
2022-06-13 16:41:34 -0400

    manage instructor design done. The blank space is supposed to have the list of all instructors.

---
+++ Deliverable11/app/src/main/java/com/example/deliverable11/manage_instructors.java
@@ -0,0 +1,14 @@
+package com.example.deliverable11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manage_instructors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_instructors);
+    }
+}
