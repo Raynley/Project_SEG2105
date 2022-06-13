dc997e7c3bbb0694c9e6369034298a673f2eaf1d
TannerGiddings21 <105750510+TannerGiddings21@users.noreply.github.com>
2022-06-13 18:00:30 -0400

    Update add_course.java

--- Deliverable11/app/src/main/java/com/example/deliverable11/add_course.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/add_course.java
@@ -3,12 +3,47 @@ package com.example.deliverable11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class add_course extends AppCompatActivity {
+    EditText course_name, course_code;
+    Button course_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+
+        course_name = findViewById(R.id.create_course_name);
+        course_code = findViewById(R.id.create_course_code);
+        course_btn = findViewById(R.id.create_course_btn);
+
+        course_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newCourseName = course_name.getText().toString().trim();
+                String newCourseCode = course_code.getText().toString().trim();
+
+                if (TextUtils.isEmpty(newCourseName) && TextUtils.isEmpty(newCourseCode))  {
+                    course_name.setError("Course name is required");
+                    course_code.setError("Course code is required");
+                    return;
+                } else if (TextUtils.isEmpty(newCourseName)) {
+                    course_name.setError("Course name is required");
+                    return;
+                } else if (TextUtils.isEmpty(newCourseCode)) {
+                    course_code.setError("Course code is required");
+                    return;
+                } else {
+                    //ENTER THE NEW COURSE INTO THE DATABASE
+                }
+            }
+        });
+
     }
+
+
 }
