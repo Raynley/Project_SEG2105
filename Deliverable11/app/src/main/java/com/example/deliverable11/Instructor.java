49ac670d1a9751ca9ad4f4326afc75a72f05adfe
TannerGiddings21 <105750510+TannerGiddings21@users.noreply.github.com>
2022-06-13 15:05:00 -0400

    Update Instructor.java

--- Deliverable11/app/src/main/java/com/example/deliverable11/Instructor.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/Instructor.java
@@ -2,7 +2,7 @@ package com.example.deliverable11;
 
 public class Instructor extends User{
 
-    public Instructor(String username, String password, int id) {
-        super(username, password, id);
+    public Instructor(String username, String password) {
+        super(username, password);
     }
 }
