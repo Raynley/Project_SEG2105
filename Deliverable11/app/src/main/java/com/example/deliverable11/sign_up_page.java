36ed9bf18071ee7807e4353537bee7d32b305fa2
Moumin <programming>
2022-06-14 17:03:46 -0400

    added the log in feature

--- Deliverable11/app/src/main/java/com/example/deliverable11/sign_up_page.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/sign_up_page.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,70 +18,74 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Objects;
-
-public class sign_up_page<userName> extends AppCompatActivity {
-    EditText userName, password, re_password;
-    Button create;
+public class sign_up_page extends AppCompatActivity {
+    EditText iname, ipassword, irepassword;
+    Button iregister;
+    TextView ilogin;
     FirebaseAuth fAuth;
-    ProgressBar bar;
+    //ProgressBar iprogressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
 
-        userName = findViewById(R.id.userName);
-        password = findViewById(R.id.password);
-        re_password = findViewById(R.id.reenter_password);
-        create = findViewById(R.id.create_the_account);
+        iname = findViewById(R.id.userName);
+        ipassword = findViewById(R.id.password);
+        irepassword = findViewById(R.id.reenter_password);
+        iregister = findViewById(R.id.create_the_account);
+        ilogin = findViewById(R.id.login);
 
         fAuth = FirebaseAuth.getInstance();
-        bar = findViewById(R.id.progressBar);
+        //iprogressbar = findViewById(R.id.progressBar);
 
         if(fAuth.getCurrentUser() != null){
-            startActivity(new Intent (getApplicationContext(), MainActivity.class));
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
         }
 
-        create.setOnClickListener(new View.OnClickListener() {
+        iregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String r_username = userName.getText().toString().trim();
-                String r_password = password.getText().toString().trim();
-                String password_2 = re_password.getText().toString().trim();
+                String name = iname.getText().toString().trim();
+                String password = ipassword.getText().toString().trim();
 
-                if(TextUtils.isEmpty(r_username)){
-                    userName.setError("Email is required");
+                if(TextUtils.isEmpty(name) == true){
+                    iname.setError("name is required");
                     return;
                 }
 
-                if(TextUtils.isEmpty(r_password)) {
-                    password.setError("Password is required");
-                    return;
+                if(TextUtils.isEmpty(password) == true){
+                    ipassword.setError("Password is required");
                 }
 
-                if(!password_2.equals(r_password)){
-                    re_password.setError("Password doesn't match");
-                    return;
+                if(password.length() < 6){
+                    ipassword.setError("Password must be >= 6 Characters");
                 }
 
-                bar.setVisibility(View.VISIBLE);
+                //iprogressbar.setVisibility(View.VISIBLE);
 
-                fAuth.createUserWithEmailAndPassword(r_username,r_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                //Register User
+                fAuth.createUserWithEmailAndPassword(name,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(sign_up_page.this, "User Created.", Toast.LENGTH_SHORT).show();
+                        if(task.isSuccessful() == true){
+                            Toast.makeText(sign_up_page.this, "User Created", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }else {
-                            Toast.makeText(sign_up_page.this, "Error ! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(sign_up_page.this, "error occured" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
 
-    }
+        ilogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
 
+    }
 }
