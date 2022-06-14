6cda3c1cff1e2c0fc61bad42386335ed811296d1
TannerGiddings21 <105750510+TannerGiddings21@users.noreply.github.com>
2022-06-14 12:06:22 -0400

    Update MainActivity.java

--- Deliverable11/app/src/main/java/com/example/deliverable11/MainActivity.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/MainActivity.java
@@ -16,14 +16,49 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username_entry, password_entry;
+    Button verify_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
 
+        username_entry = findViewById(R.id.userName);
+        password_entry = findViewById(R.id.password);
+        verify_btn = findViewById(R.id.btn_login);
+        User_Database userDatabase;
+
+        userDatabase = new User_Database(this);
+
+        verify_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Find product", Toast.LENGTH_SHORT).show();
+                String username = username_entry.getText().toString().trim();
+                String password = password_entry.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username) && TextUtils.isEmpty(password)) {
+                    username_entry.setError("Username is required");
+                    password_entry.setError("Password is required");
+                    return;
+                } else if (TextUtils.isEmpty(username)) {
+                    username_entry.setError("Username is required");
+                    return;
+                } else if (TextUtils.isEmpty(password)) {
+                    password_entry.setError("Password is required");
+                    return;
+                } else {
+
+                }
+            }
+        });
     }
 
 }
