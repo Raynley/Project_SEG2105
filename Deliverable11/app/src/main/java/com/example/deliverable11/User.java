2be81a4e56bdc57028fc7298180b5cd74d90eec5
TannerGiddings21 <105750510+TannerGiddings21@users.noreply.github.com>
2022-06-14 15:56:47 -0400

    Update User.java

--- Deliverable11/app/src/main/java/com/example/deliverable11/User.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/User.java
@@ -3,11 +3,23 @@ package com.example.deliverable11;
 public class User {
     private String username;
     private String password;
+    private int id;
+    private String type;
 
-    public User(String username, String password) {
+    public User(String username, String password, int id) {
         this.username = username;
         this.password = password;
+        this.id = id;
     }
+
+    public User(String username, String password, int id, String type) {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+        this.type = type;
+    }
+
+    public User(){}
 
     public String getUsername() {
         return username;
@@ -15,6 +27,28 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public int getId() {return id;}
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public boolean verify(String entry) {
