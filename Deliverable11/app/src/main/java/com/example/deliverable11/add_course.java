155ebb8c03c4dd7cd6408ac0ebba8575001b07b0
Moumin <programming>
2022-06-14 22:34:46 -0400

    added the log in feature

--- Deliverable11/app/src/main/java/com/example/deliverable11/add_course.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/add_course.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class add_course extends AppCompatActivity {
     EditText course_name, course_code;
     Button course_btn;
+
+    FirebaseDatabase rootCourse;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,15 @@ public class add_course extends AppCompatActivity {
                     return;
                 } else {
                     //ENTER THE NEW COURSE INTO THE DATABASE
+                    rootCourse = FirebaseDatabase.getInstance();
+                    reference = rootCourse.getReference("Courses");
+
+                    String name = course_name.getEditableText().toString();
+                    String code = course_code.getEditableText().toString();
+
+                    Course course = new Course(name, code);
+
+                    reference.child(name).setValue(course);
                 }
             }
         });
