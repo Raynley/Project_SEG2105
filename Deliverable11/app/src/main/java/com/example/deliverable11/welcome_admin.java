546835923530ed790fff515f299f7ecfd73c7370
Tanner <tgidd050@uottawa.ca>
2022-07-05 10:38:51 -0400

    welcome_instructor

--- Deliverable11/app/src/main/java/com/example/deliverable11/welcome_admin.java
+++ Deliverable11/app/src/main/java/com/example/deliverable11/welcome_admin.java
@@ -24,7 +24,7 @@ public class welcome_admin extends AppCompatActivity {
         editCourse = findViewById(R.id.edit_course_icon);
         manageInstructor = findViewById(R.id.delete_instruc_icon);
         manageStudent = findViewById(R.id.manage_stud_icon);
-        /*logout = findViewById(R.id.logout);
+        logout = findViewById(R.id.log_out);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +32,7 @@ public class welcome_admin extends AppCompatActivity {
                 Intent intent = new Intent(welcome_admin.this, MainActivity.class);
                 startActivity(intent);
             }
-        });*/
+        });
         
         addCourse.setOnClickListener(new View.OnClickListener() {
             @Override
